f431eedea1e990d400e311dbc78b32e797d1fdc1
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-07-18 20:31:38 +0100

    Create Raspberry-Pi-Blinking-Lights.js

---
+++ Raspberry-Pi-Blinking-Lights.js
@@ -0,0 +1,15 @@
+var Gpio = require("onoff").Gpio; 
+var LED = new Gpio(4, "out"); 
+var blinkInterval = setInterval(blinkLED, 500);
+function blinkLED() {
+  if (LED.readSync() === 0) {
+    LED.writeSync(1); 
+  } else {
+    LED.writeSync(0); 
+  }
+}
+function endBlink() {
+  clearInterval(blinkInterval)
+  LED.writeSync(0);
+  LED.unexport(); 
+}
